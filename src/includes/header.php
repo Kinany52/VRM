edcdc552e57a43421d7e6d7e5e62939130045837
Sarmad Al Kinany <draskinany@gmail.com>
2021-12-09 21:45:44 +0100

    Setting up proper directory structure

---
+++ src/includes/header.php
@@ -0,0 +1,52 @@
+<?php
+require 'config/config.php';
+
+
+if (isset($_SESSION['username'])) {
+	$userLoggedIn = $_SESSION['username'];
+	$user_details_quer = mysqli_query($con, "SELECT * FROM users WHERE username='$userLoggedIn'");
+	$user = mysqli_fetch_array($user_details_quer);
+} 
+else {
+	header("Location: register.php");
+}
+
+ ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Vendor Return Management</title>
+
+	<!-- JavaScript -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<script src="assets/js/bootstrap.js"></script>
+	<script src="assets/js/bootbox.min.js"></script>
+	
+	<!-- CSS -->
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+
+</head>
+<body>
+
+	<div class="top_bar">
+		
+		<div class="logo">
+			<a id="GFG" href="index.php">Vendor Return Management</a>
+		</div>
+
+		<nav>
+			<a id="GFGN" href="<?php echo $userLoggedIn; ?>">Hi there, <?php echo $user['first_name']; ?>!</a>
+			<a id="GFG" href="includes/handlers/logout.php">Logout</a>
+		</nav>
+
+
+	</div>
+
+	<div class="wrapper">
+		
+	
